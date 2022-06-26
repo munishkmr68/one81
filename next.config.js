b869862f81b45aaa74d8c9c36349ec7b27f7530e
munishkmr68 <59211593+munishkmr68@users.noreply.github.com>
2022-06-26 22:52:25 +0530

    Blank Next

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: 'build',
   reactStrictMode: true,
-  images: {
-    domains: ['fameoid.com'],
-  },
 }
 
 module.exports = nextConfig
--- pages/_app.js
+++ pages/_app.js
@@ -1,30 +1,7 @@
-
-// add bootstrap css 
-import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css'
 
-
-import App, { Container } from 'next/app';
-
-class CustomApp extends App {
-  static async getInitialProps(appContext) {
-    
-    // Call "super" to run page's `getInitialProps`
-    const appProps = await App.getInitialProps(appContext);
-
-    // Send it to `render`
-    return {
-      ...appProps
-    };
-  }
-
-  render() {
-    const { Component, pageProps } = this.props;
-
-    return (
-      <Component {...pageProps} />
-    );
-  }
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
 
-export default CustomApp;
+export default MyApp
